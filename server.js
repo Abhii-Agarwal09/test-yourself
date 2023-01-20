46503ddc223166e6c26d36b974dff0c4ade84c00
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2023-01-20 12:30:26 +0530

    initial commit, project setup

---
+++ server.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
