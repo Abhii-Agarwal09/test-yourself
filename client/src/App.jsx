f403be7cc6d450cdfb5e22510149e07c3131231b
Samriddhi Tayal <samriddhitayal@gmail.com>
2023-02-14 22:34:59 +0530

    Landing Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
-import Register from './components/Register/Register'
+import Register from './components/Register/Register';
+
 function App() {
 	return (
 		<div className='App'>
-			<Navbar/>
-			<Login/>
-			{/* <Register/> */}
+			<Navbar />
+			<Landing />
+			{/* <Login/> */}
+			{/* <Register /> */}
 			<Footer />
 		</div>
 	);
---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,42 @@
+import logo from '../../assets//landing-logo.svg';
+import './Landing.css';
+function Landing() {
+	return (
+		<>
+			<div className='description-container'>
+				<div className='description-logo-container'>
+					<img src={logo} />
+				</div>
+				<h1 className='description-heading'>Test Yourself</h1>
+				<p className='description'>
+					This app has been created to assist individuals who are color blind in
+					taking a test and adjusting the theme accordingly. Upon accessing the
+					website, the user has the option to take a test based on a specific
+					topic after configuring the necessary settings. Our goal is to create
+					a function that permits users to manage the website using voice
+					commands. Additionally, we will use webcam access to ensure the
+					validity of the test.
+				</p>
+			</div>
+			<div className='made-by-container'>
+				<p className='made-by-heading'>Made by:</p>
+				<div className='names'>
+					<ul className='names-list'>
+						<li className='name'>
+							<a href='https://github.com/SamriddhiTayal'>Samriddhi Tayal</a>
+						</li>
+						<li className='name'>
+							<a href='https://github.com/Abhii-Agarwal09'>Abhishek Agarwal</a>
+						</li>
+						<li className='name'>
+							<a href='https://github.com/priyanka-111-droid'>
+								Priyanka Govindarajan
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+}
+export default Landing;
